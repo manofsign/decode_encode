d860bbb74a1297286a019f8c37a79488679c5ba6
manofsign <manofsign@users.noreply.github.com>
2018-08-20 10:27:13 -0400

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/WebTest.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebTest.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <asp:UpdatePanel runat="server" ID="pnlMain">
+
+        <ContentTemplate>
+           <div><asp:TextBox ID="tbcInOut" runat="server" TextMode="MultiLine" Width="600px" Height="100px" ></asp:TextBox></div><br />
+            <div>
+                <asp:Button ID="btnDecode" Text="Decode" runat="server" OnClick="btnDecode_Click" /><asp:Button id="btnCopy" runat="server" OnClick="btnCopy_Click" Text="Copy" /><asp:Button runat="server" ID="btnGoToPage" Text="Go to page2" OnClick="btnGoToPage_Click" />
+            </div>
+    <div>
+    <asp:TextBox ID="tbxInput" runat="server" AutoPostBack="true" OnTextChanged="tbxInput_TextChanged" TextMode="SingleLine" Width="488px"></asp:TextBox><br />
+        </div>
+    <div><br/>
+        <asp:TextBox ID="tbxOutput" runat="server" AutoPostBack="true" TextMode="MultiLine" Width="600px" Height="100px" OnTextChanged="tbxOutput_TextChanged"></asp:TextBox>
+    </div>
+    <div>
+    <asp:Button ID="btnSubmit" runat="server" Text="Decode" OnClick="btnSubmit_Click" /><asp:Button ID="btnEncode" runat="server" OnClick="btnEncode_Click" Text="Encode" /><asp:Button ID="btnGo" runat="server" Text="GO" OnClick="btnGo_Click" /><br />
+        <asp:Label ID="lblResult" runat="server"></asp:Label><br />
+    <asp:LinkButton ID="lnkbtnCopy" runat="server"></asp:LinkButton>
+        </div>
+    <div>
+        <div><asp:TextBox ID="tbxDateDifference" runat="server" ></asp:TextBox></div>
+        <div><asp:Label ID="lblDateDifference" runat="server"></asp:Label></div>
+        <div><asp:Label ID="lblDateDifference2" runat="server"></asp:Label></div>
+        <div><asp:Label ID="lblMileage" runat="server"></asp:Label></div>
+        <div><asp:Label ID="lblMisc1" runat="server"></asp:Label></div>
+</div>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+</asp:Content>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+
+namespace WebTest
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            
+            DateTime start = new DateTime(2015, 09, 01);
+            TimeSpan span = now.Subtract(start);
+            lblDateDifference.Text = span.ToString();
+            tbxDateDifference.Text = span.TotalDays.ToString();
+            lblDateDifference2.Text = span.TotalDays.ToString();
+        }
+        
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = HttpUtility.UrlDecode(tbxInput.Text);
+            tbcInOut.Text = HttpUtility.UrlDecode(tbcInOut.Text).Substring(HttpUtility.UrlDecode(tbcInOut.Text).LastIndexOf("http") + 0);
+        }
+
+        protected void btnEncode_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = HttpUtility.UrlEncode(tbxInput.Text);
+        }
+
+        public void DateDifference(object sender, EventArgs e)
+        {
+            DateTime now = new DateTime();
+            DateTime start = new DateTime(2015, 09, 01);
+            TimeSpan span = now.Subtract(start);
+            lblDateDifference.Text = span.ToString();
+            tbxDateDifference.Text = span.TotalDays.ToString();
+
+        }
+
+        protected void tbxInput_TextChanged(object sender, EventArgs e)
+        {
+            lblMisc1.Text = HttpUtility.UrlDecode(tbxInput.Text);
+            tbxOutput.Text = HttpUtility.UrlDecode(tbxInput.Text).Substring(HttpUtility.UrlDecode(tbxInput.Text).LastIndexOf("http")+0);
+            
+            //tbxOutput.Attributes.Add("onfocus", "this.select();");
+        }
+
+        protected void btnGo_Click(object sender, EventArgs e)
+        {
+            string url = HttpUtility.UrlDecode(tbxInput.Text).Substring(HttpUtility.UrlDecode(tbxInput.Text).LastIndexOf("http") + 0);
+            Response.Redirect(url);
+            //Response.Redirect(HttpUtility.UrlDecode(tbxInput.Text).Substring(HttpUtility.UrlDecode(tbxInput.Text).LastIndexOf("http") + 0));
+        }
+
+        protected void tbxOutput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnDecode_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCopy_Click(object sender, EventArgs e)
+        {
+            tbcInOut.Focus();
+        }
+
+        protected void btnGoToPage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+         
+       
+    }
+}
